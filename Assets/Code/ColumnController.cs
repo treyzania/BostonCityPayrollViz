b741f1ca3e8bcddd08366ce9e015f303fc25f5d5
treyzania <treyzania@gmail.com>
2016-12-03 17:20:29 -0500

    Added base column controller prefab w/ associated script.

---
+++ Assets/Code/ColumnController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BostonViz {
+
+	public class ColumnController : MonoBehaviour {
+
+		public float targetHeigtScale = 1F;
+		public float lerpFactor = 1F;
+
+		void Start () {
+
+		}
+
+		void Update () {
+			this.transform.localScale = Vector3.Lerp (this.transform.localScale, new Vector3 (1f, this.targetHeigtScale, 1F), Time.deltaTime * this.lerpFactor);
+		}
+
+	}
+
+}
